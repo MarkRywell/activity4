77ff4ff2310ecde6d3803abcd5ffcbdb0b8b93a9
MarkRywell <gopro855@gmail.com>
2022-10-30 08:10:43 +0800

    named the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
+// Activity 4
+
+// Mark Rywell G. Gaje
+// John Christian Los Banos
+
 void main() {
   runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
